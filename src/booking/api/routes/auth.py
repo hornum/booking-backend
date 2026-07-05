76b13d6ea5a6f3e3b82df36391e1a67f20130076
hornum <a.chere.pnin@mail.ru>
2026-07-05 22:15:44 +0300

    fix: routers response models

--- src/booking/api/routes/auth.py
+++ src/booking/api/routes/auth.py
@@ -14,12 +14,12 @@ from booking.service.auth import register_user, login_user
 router = APIRouter(prefix="/api/v1/auth", tags=["Auth"])
 
 
-@router.post("/register", response_model=AuthResponse)
-async def register(data: UserRegister, session: AsyncSession = Depends(get_session)):
+@router.post("/register")
+async def register(data: UserRegister, session: AsyncSession = Depends(get_session)) -> AuthResponse:
     user_repo = SqlUserRepository(session)
     token_repo = SqlTokenRepository(session)
     try:
-        return await register_user(
+        user = await register_user(
             user_repo=user_repo,
             token_repo=token_repo,
             username=data.username,
@@ -29,19 +29,31 @@ async def register(data: UserRegister, session: AsyncSession = Depends(get_sessi
     except UserAlreadyExists:
         raise HTTPException(status_code=409, detail="User already exists")
 
+    return AuthResponse(
+        access_token=user.access_token,
+        refresh_token=user.refresh_token,
+        user_id=user.user_id,
+    )
 
-@router.post("/login", response_model=LoginResponse)
+
+@router.post("/login")
 async def login(
     data: Annotated[OAuth2PasswordRequestForm, Depends()],
     session: AsyncSession = Depends(get_session),
-):
+) -> LoginResponse:
     user_repo = SqlUserRepository(session)
     token_repo = SqlTokenRepository(session)
-    return await login_user(
+    user = await login_user(
         user_repo=user_repo,
         token_repo=token_repo,
         username=data.username,
         password=data.password,
+    )
+
+    return LoginResponse(
+        user_id=user.user_id,
+        access_token=user.access_token,
+        refresh_token=user.refresh_token
     )
 
 
